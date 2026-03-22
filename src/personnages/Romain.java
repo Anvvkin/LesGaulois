67ed1fe4072abf403450640ddc453cbaac6b4aca
Abdelkarim Ouaha <ouahakarim.pro@gmail.com>
2026-03-22 22:54:54 +0100

    Avancée du TP2

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,16 +1,32 @@
 package personnages;
 
 public class Romain {
+
 	private String nom;
 	private int force;
 
+	public static void main(String[] args) {
+		Romain minus = new Romain("Minus", 6);
+		
+	}
+
 	public Romain(String nom, int force) {
 		this.nom = nom;
-		this.setForce(force);
+		this.force = force;
+		assert isInvariantVerified() : "Invariant violé à la création d'un Romain !";
 	}
 
 	public String getNom() {
 		return nom;
+	}
+
+	public int getForce() {
+		return force;
+	}
+
+	public void setForce(int force) {
+		this.force = force;
+		assert isInvariantVerified() : "Invariant violé après setForce !";
 	}
 
 	public void parler(String texte) {
@@ -21,23 +37,28 @@ public class Romain {
 		return "Le romain " + nom + " : ";
 	}
 
-	public int getForce() {
-		return force;
-	}
-
-	public void setForce(int force) {
-		this.force = force;
-	}
-
 	public void recevoirCoup(int forceCoup) {
-		force = force - forceCoup;
-		if (force < 1) {
-			force = 0;
-			parler("J'abandonne !");
+		assert forceCoup > 0 : "Précondition violée : la force du coup doit être positive !";
 
+		int forceAvantCoup = force;
+
+		force -= forceCoup;
+		if (force < 0) {
+			force = 0;
+		}
+
+		assert isInvariantVerified() : "Invariant violé après recevoirCoup !";
+
+		assert force <= forceAvantCoup : "Postcondition violée : la force du Romain n'a pas diminué !";
+
+		if (force == 0) {
+			parler("J'abandonne !");
 		} else {
 			parler("Aie");
 		}
 	}
 
+	private boolean isInvariantVerified() {
+		return force >= 0;
+	}
 }
